b837c615393d61939c61952646a20c5870654314
zhaoyi <scoyzhao@163.com>
2020-10-09 20:55:21 +0800

    feat: format login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,45 @@
-import React from 'react'
-import { Button } from 'antd'
+import React, { useState } from 'react'
+import { Button, Card, Spin, Input } from 'antd'
+
+import './index.css'
 
 const Login = () => {
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const checkLogin = () => {
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  }
+
   return (
-    <div>
-      <Button type='primary'>Hello World</Button>
+    <div className='login_div'>
+      <Spin tip='Loading...' spinning={isLoading}>
+        <Card
+          style={{ width: 400 }}
+          title='scoyzhao-admin'
+          bordered={true}
+        >
+          <Input
+            className='login_input'
+            size='large'
+            placeholder='Enter your userName'
+            onChange={(e) => { setUserName(e.target.value) }}
+          />
+          <br /><br />
+          <Input.Password
+            className='login_input'
+            size='large'
+            placeholder='Enter your password'
+            onChange={(e) => { setPassword(e.target.value) }}
+          />
+          <br /><br />
+          <Button type='primary' size='large' block onClick={checkLogin}>Login in</Button>
+        </Card>
+      </Spin>
     </div>
   )
 }
