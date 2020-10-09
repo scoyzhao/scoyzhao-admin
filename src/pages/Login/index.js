34be079ff526fac4aaf0382c3cb3e54ea330ac14
zhaoyi <scoyzhao@163.com>
2020-10-09 20:13:52 +0800

    feat: project init

---
+++ src/pages/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'antd'
+
+function Login() {
+  return (
+    <div>
+      <Button type='primary'>Hello World</Button>
+    </div>
+  )
+}
+export default Login
