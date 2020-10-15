542c6687c5bbdbad09ddb897599d951c03787d5b
scoyzhao <scoyzhao@163.com>
2020-10-15 16:04:53 +0800

    feat: add axios interceptors (#1)

--- src/service/http.js
+++ src/service/http.js
@@ -2,16 +2,35 @@
  * @Author: scoyzhao
  * @Date: 2020-10-14 01:10:23
  * @Last Modified by: scoyzhao
- * @Last Modified time: 2020-10-14 17:00:48
+ * @Last Modified time: 2020-10-15 16:01:25
  */
 
 import axios from 'axios'
+import { message } from 'antd'
 
-// axios.defaults.baseURL = 'http://120.27.247.30:7001'
-axios.defaults.baseURL = process.env.NODE_ENV === 'development'
-  ? ''
-  : 'http://120.27.247.30:7001'
-axios.defaults.withCredentials = true
+const instance = axios.create({
+  baseURL: process.env.NODE_ENV === 'development'
+    ? ''
+    : 'http://120.27.247.30:7001',
+  withCredentials: true,
+})
 
-// TODO optimize axios
-export default axios
+instance.interceptors.response.use(response => {
+console.log("response", response.data)
+  // * 处理代码403
+  const {
+    code,
+    msg,
+  } = response.data
+
+  if (code === 403) {
+    message.error(msg)
+    window.location.href = '/login'
+  } else {
+    return response.data
+  }
+}, error => {
+  return Promise.reject(error)
+})
+
+export default instance
